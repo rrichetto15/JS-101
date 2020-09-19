08c6c808454c86f591cff255c76f532a9b599024
Ryan <ryan@Ryans-MacBook-Pro.local>
2020-09-19 09:51:36 -0700

    removes trailing spaces on mortgage-calculator.js

--- lesson_2/mortgage-calculator.js
+++ lesson_2/mortgage-calculator.js
@@ -11,34 +11,34 @@ function isInvalidNumber(number) {
 while (true) {
   prompt('Enter loan amount:');
   let amount = readline.question();
-  
+
   while (isInvalidNumber(amount)) {
     prompt('Please enter a positive number');
     amount = readline.question();
   }
-  
+
   prompt('Enter Annual Percentage Rate (APR):');
   let interestRate = readline.question();
-  
+
   while (isInvalidNumber(interestRate)) {
     prompt('Please enter a positive number');
     interestRate = readline.question();
   }
-  
+
   prompt('Enter loan duration (in years):');
   let years = readline.question();
-  
+
   while (isInvalidNumber(years)) {
     prompt('Please enter a positive number');
     years = readline.question();
   }
-  
+
   let annualInterestRate = Number(interestRate) / 100;
   let monthlyInterestRate = annualInterestRate / 12;
   let months = Number(years) * 12;
-  
+
   let monthlyPayment = Number(amount) * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-Number(months)))));
-  
+
   console.log(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
 
   prompt('Would you like to do another calculation?');
