a9cf5a8fef4c88a9ee4e912c51423d55d6d043de
Ryan <rrichetto@comcast.net>
2020-09-30 12:45:43 -0700

    update intro-pedac.js

--- lesson_4/intro-pedac.js
+++ lesson_4/intro-pedac.js
@@ -57,18 +57,78 @@ DATA STRUCTURE:
 Array
 
 ALGORITHM:
-- declare a result variable and initialize it to an empty array
-- create an array named substrArray that contains all of the
-  substrings of the input string that are at least 2 characters long.
-- loop through the words in the substrArray array.
-- if the word is a palindrome, append it to the result
-  array
-- return the result array
+substrings function
+=================
+  - create an empty array called `result` that will contain all required substrings
+  - create a `startingIndex` variable (value `0`) for the starting index of a substring
+  - start a loop that iterates over `startingIndex` from `0` to the length of the string minus 2
+    - create a `numChars` variable (value `2`) for the length of a substring
+    - start an inner loop that iterates over `numChars` from `2` to `string.length - startingIndex`
+      - extract a substring of length `numChars` from `string` starting at `startingIndex`
+      - append the extracted substring to the `result` array
+      - increment the `numChars` variable by `1`
+    - end the inner loop
+    - increment the `startingIndex` variable by `1`
+  - end the outer loop
+  - return the `result` array
+
+isPalindrome function
+=====================
+  - Inside the `isPalindrome` function, check whether the string
+    value is equal to its reversed value.
+
+palindromeSubstrings function
+============================
+  - declare a `result` variable and initialize it to an empty array
+  - create an array named `substrArray` that will contain all of the
+    substrings of the input string that are at least 2 characters long.
+  - loop through the words in the `substrArray` array.
+    - if the word is a palindrome, append it to the `result` array
+  - return the `result` array
 
 
-
-
-
-
+------------------
+ C - CODE (below)
+------------------
 
 */
+
+function substrings(str) {
+  let result = [];
+  let startingIndex = 0;
+
+  while (startingIndex <= str.length - 2) {
+    let numChars = 2;
+    while (numChars <= str.length - startingIndex) {
+      let substring = str.slice(startingIndex, startingIndex + numChars);
+      result.push(substring);
+      numChars += 1;
+    }
+
+    startingIndex += 1;
+  }
+
+  return result;
+}
+
+function isPalidrome(str) {
+  return str === str.split('').reverse().join('');
+}
+
+function palindromeSubstrings(str) {
+  let result = [];
+  let substringsArr = substrings(str);
+
+  substringsArr.forEach(substring => {
+    if (isPalidrome(substring)) {
+      result.push(substring);
+    }
+  });
+
+  return result;
+}
+
+console.log(palindromeSubstrings("supercalifragilisticexpialidocious")); // ["ili"]
+console.log(palindromeSubstrings("abcddcbA"));   // ["bcddcb", "cddc", "dd"]
+console.log(palindromeSubstrings("palindrome")); // []
+console.log(palindromeSubstrings(""));           // []
